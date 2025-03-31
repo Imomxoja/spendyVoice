7ab47a126cee54a28782ee4cb3c0e963bfb6e5b2
User <hodihujayevimom2006@gmail.com>
2025-03-31 21:49:04 +0500

    initial service tests

---
+++ src/test/java/org/example/service/VoiceCommandServiceTest.java
@@ -0,0 +1,66 @@
+package org.example.service;
+
+import org.example.domain.entity.VoiceCommandEntity;
+import org.example.domain.response.BaseResponse;
+import org.example.domain.response.VoiceCommandResponse;
+import org.example.repository.VoiceCommandRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.util.Optional;
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.*;
+
+class VoiceCommandServiceTest {
+
+    @InjectMocks
+    private VoiceCommandService voiceCommandService;
+
+    @Mock
+    private VoiceCommandRepository repository;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    void testDeleteWhenCommandExists() {
+        UUID id = UUID.randomUUID();
+        VoiceCommandEntity command = VoiceCommandEntity.builder().build();
+        command.setId(id);
+
+        when(repository.findById(id)).thenReturn(Optional.of(command));
+
+        BaseResponse<VoiceCommandResponse> resp = voiceCommandService.delete(id);
+
+        assertNotNull(resp);
+        assertEquals(200, resp.getStatus());
+        assertEquals("Deleted successfully", resp.getMessage());
+
+        verify(repository, times(1)).findById(id);
+        verify(repository, times(1)).deleteById(id);
+    }
+
+    @Test
+    void testDeleteWhenCommandDoesNotExist() {
+        UUID id = UUID.randomUUID();
+
+        when(repository.findById(id)).thenReturn(Optional.empty());
+
+        BaseResponse<VoiceCommandResponse> resp = voiceCommandService.delete(id);
+
+        assertNotNull(resp);
+        assertEquals(400, resp.getStatus());
+        assertEquals("Command not found", resp.getMessage());
+
+        verify(repository, times(1)).findById(id);
+        verify(repository, never()).deleteById(id);
+    }
+}
